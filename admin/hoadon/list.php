9f4256c9cf5aa1ff80d928d0fd5b43357f43117a
buiquynh-123 <quynhbtdph19396@fpt.edu.vn>
2022-11-15 11:00:02 +0700

    lan2

--- admin/hoadon/list.php
+++ admin/hoadon/list.php
@@ -34,8 +34,6 @@
                                         
                                         <td>
                                             <a onclick="return confirm('Bạn có thực sự muốn xóa không?');" href="<?php echo $xoahd ?>">
-
-                                            
                                                 <input type="button" value="Xóa">
                                             </a>
                                         </td>
